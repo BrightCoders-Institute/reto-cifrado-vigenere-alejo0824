4dc9bb1c4481a1fadbb886d1da237ee078dd3a1c
Alejandro Tellez <alejo082495@gmail.com>
2023-06-11 00:45:10 -0500

    Second part of Implemetation of encryption

--- vigenere.rb
+++ vigenere.rb
@@ -9,7 +9,7 @@ class Vigenere
 
   # Arroja el resultado en número
   def vigenere_number(word)
-    uppercase_alphabetic = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z] # Sin ñ para probar
+    uppercase_alphabetic = %w[A B C D E F G H I J K L M N Ñ O P Q R S T U V W X Y Z]
     result = Array.new(0)
 
     word.each do |letter|
@@ -18,10 +18,45 @@ class Vigenere
     result
   end
 
+  def vigenere_enc_word(num_arr)
+    uppercase_alphabetic = %w[A B C D E F G H I J K L M N Ñ O P Q R S T U V W X Y Z]
+    result = Array.new(0)
+
+    num_arr.each do |element|
+      result.push(uppercase_alphabetic[element])
+    end
+
+    puts result.inspect
+  end
+
   def encryption
     secret_word = generate_key(@message)
+    number_secret = vigenere_number(secret_word)
+    number_message = vigenere_number(@message)   
+
+    
+    sum = number_secret.zip(number_message).map {|a,b| a + b}
+    
+    number_encryption = sum.map do |element|
+      if (element > 26)
+        element - 27
+      else
+        element
+      end
+    end
+
+    puts number_encryption.inspect
+    vigenere_enc_word(number_encryption)
+    puts
+    puts secret_word.inspect  
+    puts @message.inspect
+    puts
+    puts number_secret.inspect
+    puts number_message.inspect
     
     
+  
+
   end
 
   def generate_key(word_key)
@@ -38,8 +73,8 @@ class Vigenere
   end
 end
 
-mi_vegenere = Vigenere.new('COMPutadora', 'HOLA')
+mi_vegenere = Vigenere.new('ZAPATO', 'ZONA')
 mi_vegenere.encryption
 
-mi_vegenere2 = Vigenere.new('HOLA', 'SECRETO')
-mi_vegenere2.encryption
+mi_vegenere2 = Vigenere.new('ZOLA', 'ZECRETO')
+#mi_vegenere2.encryption
