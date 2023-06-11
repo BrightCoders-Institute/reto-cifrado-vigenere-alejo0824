d8ea001e5452e4f6ae2725bb7e7ece83f69545e1
Alejandro Tellez <alejo082495@gmail.com>
2023-06-10 23:52:23 -0500

    Firts part of Implemetation of encryption

--- vigenere.rb
+++ vigenere.rb
@@ -8,7 +8,7 @@ class Vigenere
   end
 
   # Arroja el resultado en número
-  def vigenere_diagram(word)
+  def vigenere_number(word)
     uppercase_alphabetic = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z] # Sin ñ para probar
     result = Array.new(0)
 
@@ -19,17 +19,27 @@ class Vigenere
   end
 
   def encryption
-    if @key.length > @message.length
-      puts 'La palabra clave debe ser menor o igual al mensaje'
-      return
-    end
+    secret_word = generate_key(@message)
     
-    a = @message.length % @key.length
+    
+  end
 
-    puts a
-
+  def generate_key(word_key)
+    i = 0
+    secret_key = Array.new(0)
+    word_key.each do |position|
+      if(i > (@key.length - 1))
+        i = 0
+      end
+      secret_key.push(@key[i])
+      i += 1
+    end
+    secret_key
   end
 end
 
-mi_vegenere = Vigenere.new('Misb2', 'pru')
+mi_vegenere = Vigenere.new('COMPutadora', 'HOLA')
 mi_vegenere.encryption
+
+mi_vegenere2 = Vigenere.new('HOLA', 'SECRETO')
+mi_vegenere2.encryption
